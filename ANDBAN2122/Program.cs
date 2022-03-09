e34a9e86a8c600f3b1f22f8379f5829bd1ee33fe
Kevin Prieto <soporte_informactica@resistencias.com>
2022-03-09 18:17:34 +0100

    update button1_click

--- ANDBAN2122/Program.cs
+++ ANDBAN2122/Program.cs
@@ -18,5 +18,30 @@ namespace ANDBAN2122
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Alumno miAlumno = new Alumno();
+            String miAlumnoStr, miAlumnoNotaTexto;
+            miAlumno.Nombre = aluNombre.Text;
+            miAlumno.Nota = Convert.ToInt32(aluNota.Text);
+            if (miAlumno.Nota<5) {
+                miAlumnoNotaTexto = "Suspenso";
+            }
+            else if (miAlumno.Nota<7) {
+                miAlumnoNotaTexto = "Aprobado";
+            }
+            else if (miAlumno.Nota<9) {
+                miAlumnoNotaTexto = "Notable";
+            }
+            else
+                miAlumnoNotaTexto = "Sobresaliente";
+                miAlumnoStr = aluNombre.Text + " " + aluNota.Text + " " +
+                miAlumnoNotaTexto +"\n";
+                listaAlumnos.AppendText(miAlumnoStr);
+                misAlumnos.Agregar(miAlumno);
+            }
+        }
+
     }
 }
